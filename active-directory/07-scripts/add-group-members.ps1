8b5c74022e2611e37e68eaf7949b3d25e65b3fd8
Chris <111732580+Chrisos02@users.noreply.github.com>
2026-06-24 21:53:14 +0200

    Delete active-directory directory

--- active-directory/07-scripts/add-group-members.ps1
+++
@@ -1,4 +0,0 @@
-Add-ADGroupMember -Identity "IT_Users" -Members "cosmundsen"
-Add-ADGroupMember -Identity "HR_Users" -Members "ola.hr"
-Add-ADGroupMember -Identity "Sales_Users" -Members "kari.sales"
-Add-ADGroupMember -Identity "IT_Admins" -Members "cadmin"
